eada1169f21df197698f1375d0a8952e89f1c8b4
Max <maxlin2905@gmail.com>
2024-12-27 10:18:15 +1100

    Update app.py

--- app.py
+++ app.py
@@ -3,4 +3,4 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "God Blessed, Thanks God! Please bless my family a great year 2025 from staging to production"
+    return "God Blessed, Thanks God! Please bless my family a great year 2025 staing now"
